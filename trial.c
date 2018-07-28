08885ef9505d5ac0eedb09a88246d746bb88206c
dehassanglobal <hassanfridahglobal@gmail.com>
2018-07-28 12:08:00 -0500

    second commit

--- trial.c
+++ trial.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 void main(void)
 {
+//declaring some variables here
+
+int x[10];int p;
+for(p=1;p<11;p++){
+x[p]=2*p;
+printf("array[%i]\t\t%i\n",p,x[p]);
+}
 printf("this is my first uploaded file on the git\n");
+
 return ;
 }
